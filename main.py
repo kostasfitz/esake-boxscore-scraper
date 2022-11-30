fe8c1574d7398ec702252b637783e9e6a1d3ca6a
kostasfitz <kostasfitzios@gmail.com>
2022-11-30 13:52:31 +0200

    minor changes

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from boxscore_dictionary import boxscore_dictionary
 
 # scraper setup
 driver_path = 'C:\Drivers\chromedriver_win32\chromedriver.exe'
+# paste url of game boxscore
 website = 'https://www.esake.gr/el/action/EsakegameView?idgame=4180233B&mode=2'
 
+# Choose method
 # boxscore_scrape(driver_path, website)
 boxscore_dictionary(driver_path, website)
