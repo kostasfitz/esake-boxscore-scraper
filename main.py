901181c6ea1bd07dd5de55a8957eeaf6e52c230f
kostasfitz <kostasfitzios@gmail.com>
2022-11-30 13:26:54 +0200

    scraping boxscore and storing data as pandas df

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # esake scraper
 
 from boxscore_scrape import boxscore_scrape
-
+from boxscore_dictionary import boxscore_dictionary
 
 # scraper setup
 driver_path = 'C:\Drivers\chromedriver_win32\chromedriver.exe'
 website = 'https://www.esake.gr/el/action/EsakegameView?idgame=4180233B&mode=2'
 
-boxscore_scrape(driver_path, website)
+# boxscore_scrape(driver_path, website)
+boxscore_dictionary(driver_path, website)
