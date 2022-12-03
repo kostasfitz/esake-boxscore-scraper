c9851607fa4f1fa85fc1c7efd7d747e03472f653
kostasfitz <kostasfitzios@gmail.com>
2022-12-03 17:09:12 +0200

    changes to scrape all games of a fixture todo: parallelize functions bugs: produces duplicate boxscores for each game

--- main.py
+++ main.py
@@ -2,21 +2,19 @@
 
 from boxscore_scrape import boxscore_scrape
 from boxscore_dictionary import boxscore_dictionary
+from find_game_urls import find_game_urls
 
 # scraper setup
 driver_path = 'C:\Drivers\chromedriver_win32\chromedriver.exe'
-# paste url of game boxscore
-website1 = 'https://www.esake.gr/el/action/EsakegameView?idgame=F6358669&mode=2'
-website2 = 'https://www.esake.gr/el/action/EsakegameView?idgame=3B7D9406&mode=2'
-website3 = 'https://www.esake.gr/el/action/EsakegameView?idgame=F531AE99&mode=2'
-website4 = 'https://www.esake.gr/el/action/EsakegameView?idgame=41505DAC&mode=2'
-website5 = 'https://www.esake.gr/el/action/EsakegameView?idgame=B63D2BDC&mode=2'
-website6 = 'https://www.esake.gr/el/action/EsakegameView?idgame=1ED90EBF&mode=2'
+
+# paste fixture url
+website = 'https://www.esake.gr/el/action/EsakeResults?idchampionship=DC917125&idteam=&idseason=00000001&series=05'
+
+urls = find_game_urls(driver_path, website)
+
 # Choose method
 # boxscore_scrape(driver_path, website)
-boxscore_dictionary(driver_path, website1, "1")
-boxscore_dictionary(driver_path, website2, '2')
-boxscore_dictionary(driver_path, website3, '3')
-boxscore_dictionary(driver_path, website4, '4')
-boxscore_dictionary(driver_path, website5, '5')
-boxscore_dictionary(driver_path, website6, '6')
+i = 0
+for url in urls:
+    boxscore_dictionary(driver_path, url, str(i))
+    i+=1
