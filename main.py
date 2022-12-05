cc139aa0296a50874a9847cf7cb9a5f859a5ac20
kostasfitz <kostasfitzios@gmail.com>
2022-12-06 01:07:30 +0200

    changes: scraping and uploading data in parallel

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     urls = find_game_urls(driver_path, website)
 
-    # Choose method to run parallelly
+    # Choose method to run parallel
     i = 0
     processes = []
     for url in urls:
